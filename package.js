5a86fa58a50d63f649f6371745f61d75b8431fec
bslocombe <ben@techreative.com.au>
2025-09-03 13:23:47 +1000

    fix: link to updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "bslocombe:entra-oauth",
-  version: "0.0.7",
+  version: "0.0.8",
   summary: "Use Microsoft Entra to login, for Meteor 3+.",
   git: "https://github.com/bslocombe/meteor-entra-auth",
 });
